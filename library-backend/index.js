6b5f6594b3f19169388047c8a8d47835ed8bc3ec
gerardoRmrz <grmzrso@outlook.com>
2026-05-06 13:40:55 -0500

    The editAuthor was implemented to change the born year

--- library-backend/index.js
+++ library-backend/index.js
@@ -117,6 +117,7 @@ type Query {
 }
 type Mutation {
     addBook(title: String, author: String, published: Int, genre: [String]): Book 
+    editAuthor(name: String, setBornTo: Int): Author
     }
 `;
 
@@ -167,6 +168,15 @@ const resolvers = {
       };
       books = books.concat(newBook);
       return { title: newBook.title, author: newBook.author };
+    },
+    editAuthor: (_, args) => {
+      let [editedAuthor] = authors.filter((a) => a.name === args.name);
+      if (!editedAuthor) {
+        return null;
+      }
+      editedAuthor = { ...editedAuthor, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? editedAuthor : a));
+      return editedAuthor;
     },
   },
 };
