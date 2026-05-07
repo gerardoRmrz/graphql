1444c4163429650423ec37b54e708b03a7ca23b6
gerardoRmrz <grmzrso@outlook.com>
2026-05-07 09:19:40 -0500

    The backend has been refactored in multiple files

---
+++ library-backend/schema.js
@@ -0,0 +1,32 @@
+const typeDefs = /* GraphQL */ `
+  type Author {
+    id: String!
+    name: String!
+    bookCount: Int!
+    born: Int!
+  }
+  type Book {
+    id: String!
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+  }
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String!, genre: String!): [Book]
+    allAuthors: [Author]
+  }
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
+`;
+
+module.exports = typeDefs;
